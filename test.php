e92cdbf5c6c58979ebac94c4026e0eda324de07c
KoseiTanabe <newlucky0813@gmail.com>
2021-03-19 17:13:05 +0900

    first commit

---
+++ test.php
@@ -0,0 +1,14 @@
+<?php
+$name = 'nemu';
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>テスト</title>
+</head>
+<body>
+<h1>あなたの名前は</h1>
+<?php echo $name . 'です'; ?>
+</body>
+</html>
